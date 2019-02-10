17f627500d6e30c415e8baa3827f4c2e5cd3630a
RomanAverkin <romaaverkin@gmail.com>
2019-02-10 09:11:51 +0700

    Добавьте файлы проекта.

---
+++ Distance Converter/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Distance_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DistanceToConvertTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FromListBox = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ToListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConvertedDistanceLabel = new System.Windows.Forms.Label();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a distance to convert";
+            // 
+            // DistanceToConvertTextBox
+            // 
+            this.DistanceToConvertTextBox.Location = new System.Drawing.Point(183, 24);
+            this.DistanceToConvertTextBox.Name = "DistanceToConvertTextBox";
+            this.DistanceToConvertTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DistanceToConvertTextBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FromListBox);
+            this.groupBox1.Location = new System.Drawing.Point(15, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(132, 103);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "From";
+            // 
+            // FromListBox
+            // 
+            this.FromListBox.FormattingEnabled = true;
+            this.FromListBox.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.FromListBox.Location = new System.Drawing.Point(19, 26);
+            this.FromListBox.Name = "FromListBox";
+            this.FromListBox.Size = new System.Drawing.Size(94, 56);
+            this.FromListBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ToListBox);
+            this.groupBox2.Location = new System.Drawing.Point(162, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(132, 103);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "To";
+            // 
+            // ToListBox
+            // 
+            this.ToListBox.FormattingEnabled = true;
+            this.ToListBox.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.ToListBox.Location = new System.Drawing.Point(19, 26);
+            this.ToListBox.Name = "ToListBox";
+            this.ToListBox.Size = new System.Drawing.Size(94, 56);
+            this.ToListBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Coverted Distance";
+            // 
+            // ConvertedDistanceLabel
+            // 
+            this.ConvertedDistanceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ConvertedDistanceLabel.Location = new System.Drawing.Point(175, 193);
+            this.ConvertedDistanceLabel.Name = "ConvertedDistanceLabel";
+            this.ConvertedDistanceLabel.Size = new System.Drawing.Size(100, 23);
+            this.ConvertedDistanceLabel.TabIndex = 5;
+            this.ConvertedDistanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(77, 227);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(75, 23);
+            this.ConvertButton.TabIndex = 6;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(158, 227);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 7;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 267);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.ConvertedDistanceLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.DistanceToConvertTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DistanceToConvertTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox FromListBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox ToListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ConvertedDistanceLabel;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Distance Converter/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Distance_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            double enterDistance;
+            double fromConvert = 0;
+            double toConvert = 0;
+            double convertedDistance;
+
+            if (double.TryParse(DistanceToConvertTextBox.Text, out enterDistance))
+            {
+                if (FromListBox.SelectedIndex != -1 && ToListBox.SelectedIndex != -1)
+                {
+                    switch (FromListBox.SelectedItem.ToString())
+                    {
+                        case "Inches":
+                            fromConvert = (double) 1 / 36;
+                            break;
+                        case "Feet":
+                            fromConvert = (double) 1 / 3;
+                            break;
+                        case "Yards":
+                            fromConvert = 1;
+                            break;
+                    }
+
+                    switch (ToListBox.SelectedItem.ToString())
+                    {
+                        case "Inches":
+                            toConvert = 36;
+                            break;
+                        case "Feet":
+                            toConvert = 3;
+                            break;
+                        case "Yards":
+                            toConvert = 1;
+                            break;
+                    }
+
+                    convertedDistance = enterDistance * fromConvert * toConvert;
+                    ConvertedDistanceLabel.Text = convertedDistance.ToString("n2");
+                    DistanceToConvertTextBox.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Выберите оба поля!");
+                }
+            }
+            else
+            {
+                DistanceToConvertTextBox.Focus();
+                MessageBox.Show("Введите корректное число!");
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
